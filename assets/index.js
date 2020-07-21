55467f9210485083b4a84229a2cf2ec4e022f477
Elliot Wesoff <elliot.wesoff@gmail.com>
2020-07-21 14:54:27 -0700

    remove geek squad, update current job descriptions.

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var phone = ["+1", "626", "272", "7059"].join("-") // man am i paranoid or what?
+    var phone = ["+1", "626", "272", "7059"].join("-"); // man am i paranoid or what?
     var email = ["elliot", ".", "wesoff", "@", "gmail", ".com"].join("");
     var phoneElement = document.getElementById("phone");
     var emailElement = document.getElementById("email");
