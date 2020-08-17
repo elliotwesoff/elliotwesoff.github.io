5fa5ee3dcd687b28cbd102effe7ebfbf438c9f3a
Elliot Wesoff <elliot.wesoff@gmail.com>
2020-08-17 11:12:46 -0700

    minor improvements with phone/email clicks, printer friendliness

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var phone = ["+1", "626", "272", "7059"].join("-"); // man am i paranoid or what?
-    var email = ["elliot", ".", "wesoff", "@", "gmail", ".com"].join("");
+    var phone = () => [["+1", "(626)", "272"].join(" "), "7059"].join("-"); // man am i paranoid or what?
+    var email = () => ["elliot", ".", "wesoff", "@", "gmail", ".com"].join("");
     var phoneElement = document.getElementById("phone");
     var emailElement = document.getElementById("email");
-    phoneElement.addEventListener('click', function() { this.innerText = phone });
-    emailElement.addEventListener('click', function() { this.innerText = email });
+    phoneElement.addEventListener('click', function() {
+      this.innerText = phone();
+      phoneElement.setAttribute('class', 'clicked');
+    });
+    emailElement.addEventListener('click', function() {
+      this.innerText = email();
+      emailElement.setAttribute('class', 'clicked');
+    });
 });
