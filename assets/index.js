1ef1d17ae9ad91b8c0e601aecbd80394937217e1
Elliot Wesoff <elliot.wesoff@gmail.com>
2021-04-21 17:47:23 -0700

    greatly improve printer query css, add passport tech experience section, update spacing.

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var phone = () => [["+1", "(626)", "272"].join(" "), "7059"].join("-"); // man am i paranoid or what?
-    var email = () => ["elliot", ".", "wesoff", "@", "gmail", ".com"].join("");
-    var phoneElement = document.getElementById("phone");
-    var emailElement = document.getElementById("email");
-    phoneElement.addEventListener('click', function() {
-      this.innerText = phone();
-      phoneElement.setAttribute('class', 'clicked');
-    });
-    emailElement.addEventListener('click', function() {
-      this.innerText = email();
-      emailElement.setAttribute('class', 'clicked');
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  var phone = () => [["+1", "(626)", "272"].join(" "), "7059"].join("-"); // man am i paranoid or what?
+  var email = () => ["elliot", ".", "wesoff", "@", "gmail", ".com"].join("");
+  var phoneElement = document.getElementById("phone");
+  var emailElement = document.getElementById("email");
+  phoneElement.addEventListener('click', function () {
+    this.innerText = phone();
+    phoneElement.setAttribute('class', 'clicked');
+  });
+  emailElement.addEventListener('click', function () {
+    this.innerText = email();
+    emailElement.setAttribute('class', 'clicked');
+  });
+  window.onbeforeprint = () => {
+    phoneElement.click();
+    emailElement.click();
+  }
 });
