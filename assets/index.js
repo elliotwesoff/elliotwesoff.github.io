7d448dfcce2e7f895d144f23bd9aa7ec1ba4cd8b
Elliot Wesoff <ewesoff@gmail.com>
2023-07-27 10:29:14 +0200

    much needed content update.

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var phone = () => [["+1", "(626)", "272"].join(" "), "7059"].join("-"); // man am i paranoid or what?
-  var email = () => ["elliot", ".", "wesoff", "@", "gmail", ".com"].join("");
+  var phone = () => [["+1", "(626)", "650"].join(" "), "8059"].join("-");
+  var email = () => ["dev", "@", "ell10t", ".io"].join("");
   var phoneElement = document.getElementById("phone");
   var emailElement = document.getElementById("email");
   phoneElement.addEventListener('click', function () {
